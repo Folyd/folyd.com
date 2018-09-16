58d8674c74db435962d055e53c566bd094c98de6
Folyd <lyshuhow@gmail.com>
2018-09-16 15:08:07 +0800

    Add close menu button to hide navigator container on mobile platform

--- webpack/js/index.js
+++ webpack/js/index.js
@@ -1,6 +1,9 @@
-var menuButton = document.getElementById('menu-button');
-menuButton.addEventListener('click', function(event) {
+document.getElementById('menu-button').addEventListener('click', function(event) {
     var navigatorContainer = document.querySelector('.navigator-container');
     navigatorContainer.style.position = "fixed";
     navigatorContainer.style.display = "block";
+});
+document.getElementById('close-menu-button').addEventListener('click', function(event) {
+    var navigatorContainer = document.querySelector('.navigator-container');
+    navigatorContainer.style.display = "none";
 });
