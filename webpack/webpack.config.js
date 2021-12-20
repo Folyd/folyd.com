1df9f3df663052665b8564ba63beec9c882b7484
Folyd <lyshuhow@gmail.com>
2021-12-20 13:08:32 +0800

    Upgrade to webpack 5

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,52 +1,44 @@
-var webpack = require('webpack');
 var path = require('path');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  entry: {
-    'app': './js/index.js',
-    'styles': './sass/index.scss'
-  },
-  output: {
-    path: path.dirname(__dirname) + '/assets/static',
-    filename: '[name].js'
-  },
-  devtool: '#cheap-module-source-map',
-  resolve: {
-    modules: ['node_modules'],
-    extensions: ['.js']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/, exclude: /node_modules/,
-        loader: 'babel-loader'
-      },
-      {
-        test: /\.scss$/,
-        loader: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: 'css-loader!sass-loader'
-        })
-      },
-      {
-        test: /\.css$/,
-        loader: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: 'css-loader'
-        })
-      },
-      {
-        test: /\.(woff2?|ttf|eot|svg|png|jpe?g|gif)$/,
-        loader: 'file'
-      }
-    ]
-  },
-  plugins: [
-    new ExtractTextPlugin({
-      filename: 'styles.css',
-      allChunks: true
-    }),
-    new webpack.optimize.UglifyJsPlugin()
-  ]
+var options = {
+    entry: {
+        'app': './js/index.js',
+        'styles': './sass/index.scss'
+    },
+    output: {
+        path: path.dirname(__dirname) + '/assets/static',
+        filename: '[name].js'
+    },
+    devtool: 'cheap-module-source-map',
+    mode: 'production',
+    resolve: {
+        modules: ['node_modules'],
+        extensions: ['', '.js']
+    },
+    module: {
+        rules: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.woff2?$|\.ttf$|\.eot$|\.svg$|\.png|\.jpe?g\|\.gif$/,
+                use: ['file']
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({ filename: 'styles.css' })
+    ],
 };
+
+module.exports = options;
